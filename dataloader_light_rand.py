5d87644ea737f5051bf74999d25201ae721c2ec0
sharik-siddiqi <103926583+sharik-siddiqi@users.noreply.github.com>
2022-04-18 16:07:06 +0200

    Update dataloader_light_rand.py

--- dataloader_light_rand.py
+++ dataloader_light_rand.py
@@ -77,7 +77,8 @@ class Surr12kModelNetDataLoader(Dataset):
         self.npoints = npoint
         self.rand = rand
         self.split = split
-
+         
+        # Load the simplified mesh
         _, self.f = igl.read_triangle_mesh("/home/raml_sharik/Diff-FMAPs-PyTorch-main/data/3973_simp_1000.obj")
         
         # Matching co-ordinates from low density to high density meshes (for dealing with low density meshes (2100 -> 1000))
@@ -88,57 +89,37 @@ class Surr12kModelNetDataLoader(Dataset):
         # Matching co-ordinates from low density to high density meshes (for dealing with low density meshes (6890 -> 2100))
         match = loadmat("/home/raml_sharik/Diff-FMAPs-PyTorch-main/data/match_6890_2100 (1).mat", variable_names = ['match'])
         match = match['match'][:,0] - 1;
+        
+        num_test = 2 # Number of test data examples per sub-divided case (if 10 files, therefore total dataset size = 2*10 = 20)
+        
+        # A certain section of shapes taken from a bigger dataset ('vert_surreal_same.mat' & 'vert_bent_same.mat') (1000 shapes out of possible 4000 shapes)
+        # {Work with the data you prepared as per the requirements given on the repository homepage readme.md}
+        -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-        # Divided datasets containing the heat kernel data at t=0.005 sec and 2100 vertices/mesh
-        indices_0 = loadmat('./data/unsup_data_t_0.005_hk_0_same.mat', variable_names = ['indices'])
-        indices_1 = loadmat('./data/unsup_data_t_0.005_hk_1_same.mat', variable_names = ['indices'])
-        indices_2 = loadmat('./data/unsup_data_t_0.005_hk_2_same.mat' ,variable_names = ['indices'] )
-        indices_3 = loadmat('./data/unsup_data_t_0.005_hk_3_same.mat' ,variable_names = ['indices'])
-        indices_4 = loadmat('./data/unsup_data_t_0.005_hk_4_same.mat' ,variable_names = ['indices'])
-        indices_5 = loadmat('./data/unsup_data_t_0.005_hk_5_same.mat' ,variable_names = ['indices'])
-        indices_6 = loadmat('./data/unsup_data_t_0.005_hk_6_same.mat' ,variable_names = ['indices'])
-        indices_7 = loadmat('./data/unsup_data_t_0.005_hk_7_same.mat', variable_names = ['indices'])
-        indices_8 = loadmat('./data/unsup_data_bent_t_0.005_hk_0_same.mat' ,variable_names = ['indices'])
-        indices_9 = loadmat('./data/unsup_data_bent_t_0.005_hk_1_same.mat' ,variable_names = ['indices'])
-
-        # Overall dataset containg 4000 shapes
-        data_1 = loadmat(os.path.join(root, 'vert_surreal_same.mat'), variable_names = ['vert'])
-        data_2 = loadmat(os.path.join(root, 'vert_bent_same.mat'), variable_names = ['vert'])
+        # Divided datasets containing the indices (cases taken from a bigger dataset) and heat kernel data at t=0.005 sec and 2100 vertices/mesh
+        # indices_0 = loadmat('./data/unsup_data_t_0.005_hk_0_same.mat', variable_names = ['indices'])
+        # so on until indices_9 (10 files)
+        # Overall dataset containg 4000 shapes (vertices and faces)
+        # data_1 = loadmat(os.path.join(root, 'vert_surreal_same.mat'), variable_names = ['vert'])
+        # data_2 = loadmat(os.path.join(root, 'vert_bent_same.mat'), variable_names = ['vert'])
 
         # Splitting into training and test dataset
-        if (split == 'train'):
-            data_0_train = data_1['vert'][indices_0['indices'][0,:-2].reshape(-1,), :, :]
-            data_1_train = data_1['vert'][indices_1['indices'][0,:-2].reshape(-1,), :, :]
-            data_2_train = data_1['vert'][indices_2['indices'][0,:-2].reshape(-1,), :, :]
-            data_3_train = data_1['vert'][indices_3['indices'][0,:-2].reshape(-1,), :, :]
-            data_4_train = data_1['vert'][indices_4['indices'][0,:-2].reshape(-1,), :, :]
-            data_5_train = data_1['vert'][indices_5['indices'][0,:-2].reshape(-1,), :, :]
-            data_6_train = data_1['vert'][indices_6['indices'][0,:-2].reshape(-1,), :, :]
-            data_7_train = data_1['vert'][indices_7['indices'][0,:-2].reshape(-1,), :, :]
-            data_bent_1_train = data_2['vert'][indices_8['indices'][0,:-2].reshape(-1,), :, :]
-            data_bent_2_train = data_2['vert'][indices_9['indices'][0,:-2].reshape(-1,), :, :]
+        # if (split == 'train'):
+        #    data_0_train = data_1['vert'][indices_0['indices'][0,:-num_test].reshape(-1,), :, :]
+        #    data_bent_1_train = data_2['vert'][indices_8['indices'][0,:-num_test].reshape(-1,), :, :]
 
-        else:
-            data_0_train = data_1['vert'][indices_0['indices'][0,-2:].reshape(-1,), :, :]
-            data_1_train = data_1['vert'][indices_1['indices'][0,-2:].reshape(-1,), :, :]
-            data_2_train = data_1['vert'][indices_2['indices'][0,-2:].reshape(-1,), :, :]
-            data_3_train = data_1['vert'][indices_3['indices'][0,-2:].reshape(-1,), :, :]
-            data_4_train = data_1['vert'][indices_4['indices'][0,-2:].reshape(-1,), :, :]
-            data_5_train = data_1['vert'][indices_5['indices'][0,-2:].reshape(-1,), :, :]
-            data_6_train = data_1['vert'][indices_6['indices'][0,-2:].reshape(-1,), :, :]
-            data_7_train = data_1['vert'][indices_7['indices'][0,-2:].reshape(-1,), :, :]
-            data_bent_1_train = data_2['vert'][indices_8['indices'][0,-2:].reshape(-1,), :, :]
-            data_bent_2_train = data_2['vert'][indices_9['indices'][0,-2:].reshape(-1,), :, :]
+        # else:
+        #    data_0_test = data_1['vert'][indices_0['indices'][0,-num_test:].reshape(-1,), :, :]
+        #    data_bent_1_test = data_2['vert'][indices_8['indices'][0,-num_test:].reshape(-1,), :, :]
+ 
 
-        if split =="train":
-            print("train")
-            self.data = np.concatenate((data_0_train[:,match,:], data_1_train[:,match,:], data_2_train[:,match,:], data_3_train[:,match,:], data_4_train[:,match,:], data_5_train[:,match,:], data_6_train[:,match,:], data_7_train[:,match,:], data_bent_1_train[:,match,:], data_bent_2_train[:,match,:]),axis=0).astype(dtype=np.float32)
-            #self.data = np.concatenate((data_0_train, data_1_train, data_2_train, data_3_train, data_4_train, data_5_train, data_6_train, data_7_train, data_bent_1_train, data_bent_2_train),axis=0).astype(dtype=np.float32)
+        # if split =="train":
+        #    self.data -> concatenate all the files designated for training (data_0_train,  data_bent_1_train.....etc)
 
-        if split =="test":
-            print("test")
-            self.data = np.concatenate((data_0_train[:,match,:], data_1_train[:,match,:], data_2_train[:,match,:], data_3_train[:,match,:], data_4_train[:,match,:], data_5_train[:,match,:], data_6_train[:,match,:], data_7_train[:,match,:], data_bent_1_train[:,match,:], data_bent_2_train[:,match,:]),axis=0).astype(dtype=np.float32)
-            #self.data = np.concatenate((data_0_train, data_1_train, data_2_train, data_3_train, data_4_train, data_5_train, data_6_train, data_7_train, data_bent_1_train, data_bent_2_train),axis=0).astype(dtype=np.float32)
+        # if split =="test":
+        #    self.data -> concatenate all the files designated for validation (data_0_test,  data_bent_1_test.....etc)
+
+        ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     def __len__(self):
         return len(self.data)
